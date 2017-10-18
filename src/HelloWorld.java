d26242364957ba7a32a1b162c451d9b41bd68aa3
LimFull <32320720+LimFull@users.noreply.github.com>
2017-10-18 14:15:18 +0900

    remote update

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Hello Hotfix-XXXXX");
 		System.out.println("Hello Masthher-XXXXX");
+		System.out.println("Hello remote heyheyhey");
 
 	}
  

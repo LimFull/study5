090bc2d4e7da7855163e2eb55f2930851b8bca72
LimFull <32320720+LimFull@users.noreply.github.com>
2017-10-18 14:48:24 +0900

    4545454545

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
 		System.out.println("Hello Masthher-XXXXX");
 		System.out.println("Hello local hohoho");
 		System.out.println("Hello remote heyheyhey");
-		/*Pusdsdsdll demo*/
+		/*Pusdsdsdlhgoooo454545454545454545454545o*/
 	}
  
 }
